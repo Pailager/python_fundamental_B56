5dcb898884af7a7388ca3d6d77b8a59d85b1225b
Pailager <37395065+Pailager@users.noreply.github.com>
2022-01-12 07:46:04 +0530

    Day1_B56 work

---
+++ Day1_B56.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello all welcome to the python world!")
+
+
+# In[2]:
+
+
+a=1
+print(a)
+
+
+# In[ ]:
+
+
+File-> downlodsas ->notbook.ipynb
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
